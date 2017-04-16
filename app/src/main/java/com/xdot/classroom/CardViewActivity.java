b954902fdf8a52ec3abdcec308d7b167a2a37301
ArteneR <georgevici.ioan@yahoo.com>
2017-04-16 16:30:17 +0300

    Made the list for Schedules Activity to work

--- app/src/main/java/com/xdot/classroom/CardViewActivity.java
+++ app/src/main/java/com/xdot/classroom/CardViewActivity.java
@@ -1,44 +1,25 @@
-package com.xdot.classroom.screens;
+package com.xdot.classroom;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.xdot.classroom.DataObject;
-import com.xdot.classroom.MyRecyclerViewAdapter;
-import com.xdot.classroom.R;
-
 import java.util.ArrayList;
 
 
-public class SchedulesActivity extends AppCompatActivity {
+public class CardViewActivity extends AppCompatActivity {
 
-    final String TAG = "MY_DEBUG";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static String LOG_TAG = "CardViewActivity";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedules);
-
-        activateCustomActionBar();
+        setContentView(R.layout.activity_card_view);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -75,37 +56,5 @@ public class SchedulesActivity extends AppCompatActivity {
             results.add(index, obj);
         }
         return results;
-    }
-
-
-    private void activateCustomActionBar() {
-        android.support.v7.app.ActionBar mActionBar = getSupportActionBar();
-
-        mActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#BB266195")));
-        mActionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#BB266195")));
-
-        mActionBar.setDisplayShowHomeEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
-        LayoutInflater mInflater = LayoutInflater.from(this);
-
-        View mCustomView = mInflater.inflate(R.layout.custom_actionbar, null);
-
-        mActionBar.setCustomView(mCustomView);
-        mActionBar.setDisplayShowCustomEnabled(true);
-    }
-
-
-    public void clicked(View view) {
-        Log.d(TAG, "Button clicked!");
-
-        switch (view.getId()) {
-            case R.id.imageView1:
-                Log.d(TAG, "Button: Back");
-                break;
-
-            case R.id.imageView2:
-                Log.d(TAG, "Button: Add Schedule");
-                break;
-        }
     }
 }

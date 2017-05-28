ff30a90e8f7cac104483f195fece643e387885e9
ArteneR <georgevici.ioan@yahoo.com>
2017-05-28 16:24:09 +0300

    Update deviceRegistrationId when token changes

--- app/src/main/java/com/xdot/classroom/send_notifications/FirebaseIDService.java
+++ app/src/main/java/com/xdot/classroom/send_notifications/FirebaseIDService.java
@@ -1,34 +1,44 @@
 package com.xdot.classroom.send_notifications;
 
 import android.util.Log;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 
+
 public class FirebaseIDService extends FirebaseInstanceIdService {
 
-        private static final String TAG = "FirebaseIDService";
+        private static String LOG_TAG = "FirebaseIDService";
+        private FirebaseDatabase firebaseDB;
+        private DatabaseReference firebaseDBRef;
 
 
         @Override
         public void onTokenRefresh() {
                 // Get updated InstanceID token.
                 String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-                Log.d(TAG, "Refreshed token: " + refreshedToken);
+                Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
 
+                connectToFirebase();
                 // TODO: Implement this method to send any registration to your app's servers.
                 sendRegistrationToServer(refreshedToken);
         }
 
-        /**
-         * Persist token to third-party servers.
-         *
-         * Modify this method to associate the user's FCM InstanceID token with any server-side account
-         * maintained by your application.
-         *
-         * @param token The new token.
-         */
-        private void sendRegistrationToServer(String token) {
+
+
+        private void sendRegistrationToServer(String newDeviceRegistrationId) {
                 // Add custom implementation, as needed.
+                String userId = "4o5JWilDQyTcrY7JyngUhzR8NGj1";
+                firebaseDBRef.child("Users").child(userId).child("DeviceRegistrationID").setValue(newDeviceRegistrationId);
+        }
+
+
+
+        private void connectToFirebase() {
+                Log.d(LOG_TAG, "connectToFirebase");
+                firebaseDB = FirebaseDatabase.getInstance();
+                firebaseDBRef = firebaseDB.getReference();
         }
 }

be13fd81bd931208e23f46a373cfe3acc1c4fd16
ArteneR <georgevici.ioan@yahoo.com>
2017-06-02 01:17:35 +0300

    Refurbished the ActivateAccount activity

---
+++ app/src/main/java/com/xdot/classroom/screens/activate_account/ActivateAccountActivity.java
@@ -0,0 +1,75 @@
+package com.xdot.classroom.screens.activate_account;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.xdot.classroom.R;
+
+
+
+public class ActivateAccountActivity extends AppCompatActivity {
+        private static String LOG_TAG = "ActivateAccountActivity";
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_activate_account);
+
+                activateCustomActionBar();
+        }
+
+
+        /*
+         * Setup the custom action bar for navigation
+         */
+        private void activateCustomActionBar() {
+                android.support.v7.app.ActionBar mActionBar = getSupportActionBar();
+
+                mActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#BB266195")));
+                mActionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#BB266195")));
+
+                mActionBar.setDisplayShowHomeEnabled(false);
+                mActionBar.setDisplayShowTitleEnabled(false);
+                LayoutInflater mInflater = LayoutInflater.from(this);
+
+                View mCustomView = mInflater.inflate(R.layout.custom_actionbar_activate_account, null);
+
+                mActionBar.setCustomView(mCustomView);
+                mActionBar.setDisplayShowCustomEnabled(true);
+        }
+
+
+        /*
+         * Handle user click events
+         */
+        public void clicked(View view) {
+                Log.d(LOG_TAG, "Button clicked!");
+
+                switch (view.getId()) {
+                    case R.id.ivLeftActionbarButton:
+                        Log.d(LOG_TAG, "Button: Back");
+                        goToPreviousActivity();
+                        break;
+
+                    case R.id.btnCreateAccount:
+                        Log.d(LOG_TAG, "Button: Create Account");
+                        createAccount();
+                        break;
+                }
+        }
+
+
+        private void goToPreviousActivity() {
+                super.onBackPressed();
+        }
+
+
+        private void createAccount() {
+                Log.d(LOG_TAG, "Creating account...");
+        }
+}

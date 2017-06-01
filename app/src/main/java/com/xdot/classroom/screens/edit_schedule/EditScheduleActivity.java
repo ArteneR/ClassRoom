b909acafc7907fedf88705a607c79a5c68bee57b
ArteneR <georgevici.ioan@yahoo.com>
2017-06-01 19:03:54 +0300

    Handle button click events on EditSchedule activity

--- app/src/main/java/com/xdot/classroom/screens/edit_schedule/EditScheduleActivity.java
+++ app/src/main/java/com/xdot/classroom/screens/edit_schedule/EditScheduleActivity.java
@@ -1,5 +1,8 @@
 package com.xdot.classroom.screens.edit_schedule;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +10,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import com.xdot.classroom.CommonFunctionalities;
 import com.xdot.classroom.R;
+import com.xdot.classroom.screens.schedules.SchedulesActivity;
 
 
 
@@ -55,12 +60,57 @@ public class EditScheduleActivity extends AppCompatActivity {
                         Log.d(LOG_TAG, "Button: Back");
                         goToPreviousActivity();
                         break;
+
+                    case R.id.btnSaveChanges:
+                        Log.d(LOG_TAG, "Button: Save Changes");
+                        saveChanges();
+                        break;
+
+                    case R.id.btnDeleteSchedule:
+                        Log.d(LOG_TAG, "Button: Delete Schedule");
+                        openDeleteScheduleConfirmation();
+                        break;
                 }
         }
 
 
         private void goToPreviousActivity() {
                 super.onBackPressed();
+        }
+
+
+        private void goToSchedulesActivity() {
+                Intent intent = new Intent(this, SchedulesActivity.class);
+                this.startActivity(intent);
+        }
+
+
+        private void saveChanges() {
+                Log.d(LOG_TAG, "Saving changes...");
+        }
+
+
+        private void openDeleteScheduleConfirmation() {
+                new AlertDialog.Builder(this, AlertDialog.THEME_HOLO_DARK)
+                    .setTitle("Confirmation")
+                    .setMessage("Do you really want to delete this schedule?")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                    deleteSchedule();
+                                    CommonFunctionalities.displayShortToast("Schedule has been successfully deleted!", getApplicationContext());
+                                    goToSchedulesActivity();
+                            }})
+                    .setNegativeButton(android.R.string.no, null)
+                    .show();
+        }
+
+
+        private void deleteSchedule() {
+                Log.d(LOG_TAG, "Deleting schedule...");
+
+
+
         }
 
 }

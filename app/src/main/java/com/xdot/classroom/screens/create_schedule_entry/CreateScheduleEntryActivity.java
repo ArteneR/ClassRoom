619199d8e228681ba8433a65cc7f9dba6630da32
ArteneR <georgevici.ioan@yahoo.com>
2017-06-05 22:54:47 +0300

    Fixed bug with selecting start and end time

--- app/src/main/java/com/xdot/classroom/screens/create_schedule_entry/CreateScheduleEntryActivity.java
+++ app/src/main/java/com/xdot/classroom/screens/create_schedule_entry/CreateScheduleEntryActivity.java
@@ -1,5 +1,6 @@
 package com.xdot.classroom.screens.create_schedule_entry;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +12,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.xdot.classroom.CommonFunctionalities;
+import com.xdot.classroom.DataProvider;
 import com.xdot.classroom.R;
+import com.xdot.classroom.screens.edit_schedule_entry.fragments.EndTimePickerFragment;
+import com.xdot.classroom.screens.edit_schedule_entry.fragments.StartTimePickerFragment;
+import com.xdot.classroom.screens.schedules.SchedulesActivity;
+import com.xdot.classroom.university_activities.UniversityActivity;
 
 
 
 public class CreateScheduleEntryActivity extends AppCompatActivity {
         private static String LOG_TAG = "CodeTemplateActivity";
-        private String selectedDayOfTheWeek;
+        private DataProvider dataProvider;
+        private FirebaseDatabase firebaseDB;
+        private DatabaseReference firebaseDBRef;
+        private FirebaseAuth firebaseAuth;
         private Spinner spinnerDayOfTheWeek;
         private Spinner spinnerEntryType;
         private EditText etSubjectName;
@@ -25,12 +38,22 @@ public class CreateScheduleEntryActivity extends AppCompatActivity {
         private Button btnEndTime;
         private EditText etBuildingName;
         private EditText etRoomName;
+        private String selectedDayOfTheWeek;
+        private String selectedEntryType;
+        private String selectedSubjectName;
+        private String selectedStartTime;
+        private String selectedEndTime;
+        private String selectedBuildingName;
+        private String selectedRoomName;
 
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_create_schedule_entry);
+
+                connectToFirebase();
+                initializeDataProviderModule();
 
                 selectedDayOfTheWeek = getIntent().getStringExtra("selected_day_of_week");
 
@@ -41,6 +64,18 @@ public class CreateScheduleEntryActivity extends AppCompatActivity {
                 activateEntryTypeSpinner();
 
                 fillElementsWithData();
+        }
+
+
+        private void connectToFirebase() {
+                firebaseDB = FirebaseDatabase.getInstance();
+                firebaseDBRef = firebaseDB.getReference();
+                firebaseAuth = FirebaseAuth.getInstance();
+        }
+
+
+        private void initializeDataProviderModule() {
+                dataProvider = (DataProvider) getApplication();
         }
 
 
@@ -123,12 +158,77 @@ public class CreateScheduleEntryActivity extends AppCompatActivity {
                                 goToPreviousActivity();
                                 break;
 
+                        case R.id.btnStartTime:
+                                Log.d(LOG_TAG, "Button: Start Time");
+                                showStartTimePickerDialog();
+                                break;
+
+                        case R.id.btnEndTime:
+                                Log.d(LOG_TAG, "Button: End Time");
+                                showEndTimePickerDialog();
+                                break;
+
+                        case R.id.btnCreateScheduleEntry:
+                                Log.d(LOG_TAG, "Button: Create Schedule Entry");
+                                createScheduleEntry();
+                                break;
                 }
         }
 
 
         private void goToPreviousActivity() {
                 super.onBackPressed();
+        }
+
+
+        private void goToSchedulesActivity() {
+                Intent intent = new Intent(this, SchedulesActivity.class);
+                this.startActivity(intent);
+        }
+
+
+        private void showStartTimePickerDialog() {
+                StartTimePickerFragment newFragment = new StartTimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "startTimePicker");
+        }
+
+
+        private void showEndTimePickerDialog() {
+                EndTimePickerFragment newFragment = new EndTimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "endTimePicker");
+        }
+
+
+        private void createScheduleEntry() {
+                getUserSelectedValues();
+
+                String userId = firebaseAuth.getCurrentUser().getUid();
+                String scheduleId = dataProvider.getCurrentScheduleId();
+                String dayOfWeek = CommonFunctionalities.capitalizeFirstLetter(selectedDayOfTheWeek);
+                String newEntryType = CommonFunctionalities.singularToPlural(selectedEntryType);
+
+                DatabaseReference newScheduleEntryRef = firebaseDBRef.child("Users").child(userId).child("Schedules").child(scheduleId).child("Entries").child(dayOfWeek).child(newEntryType).push();
+                UniversityActivity newUnivActivity = new UniversityActivity("UNDEFINED_INDEX",
+                                                                            selectedStartTime,
+                                                                            selectedEndTime,
+                                                                            selectedRoomName,
+                                                                            selectedBuildingName,
+                                                                            selectedSubjectName);
+                newScheduleEntryRef.setValue(newUnivActivity);
+
+                CommonFunctionalities.displayShortToast("Changes have been successfully saved!", getApplicationContext());
+                goToSchedulesActivity();
+        }
+
+
+        private void getUserSelectedValues() {
+                selectedDayOfTheWeek = spinnerDayOfTheWeek.getSelectedItem().toString();
+                selectedEntryType = spinnerEntryType.getSelectedItem().toString();
+                selectedSubjectName = etSubjectName.getText().toString();
+                selectedStartTime = btnStartTime.getText().toString();
+                selectedEndTime = btnEndTime.getText().toString();
+                selectedBuildingName = etBuildingName.getText().toString();
+                selectedRoomName = etRoomName.getText().toString();
         }
 
 }
